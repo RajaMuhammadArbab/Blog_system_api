0f780aa727b579bab7e7c96fbc118b2327838e81
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-08-02 13:32:31 +0500

    Add files via upload

---
+++ project/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('posts/' , views.post_list),
+    path('posts/<int:pk>/' , views.post_detail),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+ 
