1b911ef99967daab55c13fa97ee2351483bdc35d
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-08-08 12:49:02 +0500

    Delete project directory

--- project/blog/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'  # or ['id', 'title', 'content', 'author']
